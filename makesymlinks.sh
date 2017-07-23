36fa145be1df0a56a7d6cb780e3da645f2b4ccc4
Blaise Thompson <blaise@untzag.com>
2017-07-23 17:07:06 -0500

    spacemacs

--- makesymlinks.sh
+++ makesymlinks.sh
@@ -1,16 +1,12 @@
 #!/bin/bash
-############################
-# .make.sh
-# This script creates symlinks from the home directory to any desired dotfiles in ~/dotfiles
-############################
 
-########## Variables
+########## define
 
-dir=~/dotfiles                    # dotfiles directory
-olddir=~/dotfiles_old             # old dotfiles backup directory
-files=""    # list of files/folders to symlink in homedir
+dir=~/dotfiles  # dotfiles directory
+olddir=~/dotfiles_old  # old dotfiles backup directory
+files="spacemacs"  # list of files/folders to symlink in homedir
 
-##########
+########## do
 
 # create dotfiles_old in homedir
 echo -n "Creating $olddir for backup of any existing dotfiles in ~ ..."
