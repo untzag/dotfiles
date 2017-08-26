5eb01e5962ba32416e597868f603b79304a61b9c
Blaise Thompson <blaise@untzag.com>
2017-08-25 23:54:39 -0500

    improve link.sh

--- link.sh
+++ link.sh
@@ -1,27 +1,16 @@
 #!/bin/bash
 
-########## define
-
-dir=~/dotfiles  # dotfiles directory
-olddir=~/dotfiles_old  # old dotfiles backup directory
-files="spacemacs"  # list of files/folders to symlink in homedir
-
-########## do
-
 # create dotfiles_old in homedir
-echo -n "Creating $olddir for backup of any existing dotfiles in ~ ..."
+olddir=~/dotfiles_old
 mkdir -p $olddir
-echo "done"
 
 # change to the dotfiles directory
-echo -n "Changing to the $dir directory ..."
+dir=~/dotfiles/dotfiles
 cd $dir
-echo "done"
 
-# move any existing dotfiles in homedir to dotfiles_old directory, then create symlinks from the homedir to any files in the ~/dotfiles directory specified in $files
-for file in $files; do
-    echo "Moving any existing dotfiles from ~ to $olddir"
-    mv ~/.$file ~/dotfiles_old/
-    echo "Creating symlink to $file in home directory."
-    ln -s $dir/$file ~/.$file
+# move current files and link contained files
+files=$(ls $dir)
+for name in $files; do
+    mv ~/.$name ~/dotfiles_old/
+    ln -s $dir/$name ~/.$name
 done
