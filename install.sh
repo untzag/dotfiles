e499bec4763c32de27e7c8b76dd63d524973a061
Blaise Thompson <blaise@untzag.com>
2017-09-28 15:11:20 -0500

    biber

--- install.sh
+++ install.sh
@@ -1,19 +1,31 @@
-#function install{
-#  yaourt -S $1 --needed --noconfirm
-#  # clear temp
-#  /bin/rm -rf /tmp/* /tmp/.* &>/dev/null
-#}
+function printColor {
+    YELLOW='\033[0;33m'
+    NC='\033[0m' # No Color
+    printf "${YELLOW}$*${NC}\n"
+}
 
+function printLine {
+    printColor "%`tput cols`s"|tr ' ' '#'
+}
+
+function install {
+  printLine
+  printColor $1
+  yaourt -S $1 --needed --noconfirm
+  # clear temp
+  /bin/rm -rf /tmp/* /tmp/.* &>/dev/null
+}
 
 # utilities
 sudo pacman -S git --needed
 sudo pacman -S xterm --needed
 sudo pacman -S yaourt --needed
-sudo pacman -S xclip --needed
-sudo pacman -S vim --needed
-sudo pacman -S imagemagick --needed
-sudo pacman -S scrot --needed
-yaourt -S insync --needed
+install xclip
+install vim
+install imagemagick
+install scrot
+install insync
+install ranger
 
 # i3
 yaourt -S i3-gaps --needed
@@ -25,7 +37,8 @@ yaourt -S i3lock-color-git --needed
 yaourt -S anaconda --needed --tmp ~/Desktop
 
 # applications
-yaourt -S texlive-most --needed --noconfirm
-sudo pacman -S emacs --needed
+install texlive-most
+install biber
+install emacs
 git clone https://github.com/syl20bnr/spacemacs ~/.emacs.d
-yaourt -S slack-desktop --needed
+install slack-desktop
